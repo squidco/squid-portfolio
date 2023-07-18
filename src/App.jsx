eb9a1d26dd25a3cb8687c448ad12b98bd098c60e
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-07-18 14:40:05 -0700

    Fixed the mobile view being zoomed in (it technically was not) and removed some useless styling.

--- src/App.jsx
+++ src/App.jsx
@@ -30,70 +30,68 @@ function App() {
 
   return (
     <>
-      <Nav />
-      <ContentContainer id="about">
-        <h1 className="outline">Howdy,</h1>
-        <p>
-          I'm Anthony Pillow: photographer, rock climber, and full stack web
-          developer. If I'm not spending time on my hobbies I'm usually hanging
-          out with friends or exploring abandoned places.
-        </p>
-        <p>
-          I started coding after following courses on Udemy and decided to
-          pursue coding through a formal bootcamp hosted at UC Davis Continuing
-          and Professional Education. After completing the 6 month course I went
-          on to join the company that provided the course as an Instructional
-          Specialist and have been keeping my skills sharp while teaching new
-          developers how to code.
-        </p>
-        <p>
-          Below are my projects, resume and skills info, and how to contact me
-          with any inquiries.
-        </p>
-      </ContentContainer>
-      <ContentContainer id="projects">
-        <h1 className="outline">Projects</h1>
-        <ProjectContainer projectArray={projects} />
-      </ContentContainer>
-
-      <ContentContainer id="resume">
-        <div>
-          <h1 className="outline">Skills and Resume</h1>
-          <a href={Resume} download className="nav-links">
-            <FontAwesomeIcon icon={faDownload} /> Download Resume
-          </a>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-around",
-            width: "100%",
-          }}
-        ></div>
-      </ContentContainer>
-
-      <ContentContainer id="contact">
-        <h1 className="outline">Contact Me</h1>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "space-evenly",
-          }}
-        >
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <ContentContainer id="about">
+          <h1 className="outline">Howdy,</h1>
           <p>
-            <span>
-              <FontAwesomeIcon icon={faEnvelope} /> anthonypillow18@gmail.com
-            </span>
+            I'm Anthony Pillow: photographer, rock climber, and full stack web
+            developer. If I'm not spending time on my hobbies I'm usually
+            hanging out with friends or exploring abandoned places.
           </p>
           <p>
-            <span>
-              <FontAwesomeIcon icon={faGithub} /> SquidDOTjpeg
-            </span>
+            I started coding after following courses on Udemy and decided to
+            pursue coding through a formal bootcamp hosted at UC Davis
+            Continuing and Professional Education. After completing the 6 month
+            course I went on to join the company that provided the course as an
+            Instructional Specialist and have been keeping my skills sharp while
+            teaching new developers how to code.
           </p>
-        </div>
-      </ContentContainer>
+          <p>
+            Below are my projects, resume and skills info, and how to contact me
+            with any inquiries.
+          </p>
+        </ContentContainer>
+        <ContentContainer id="projects">
+          <h1 className="outline">Projects</h1>
+          <ProjectContainer projectArray={projects} />
+        </ContentContainer>
+
+        <ContentContainer id="resume">
+          <div>
+            <h1 className="outline">Skills and Resume</h1>
+            <a href={Resume} download className="nav-links">
+              <FontAwesomeIcon icon={faDownload} /> Download Resume
+            </a>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              width: "100%",
+            }}
+          ></div>
+        </ContentContainer>
+
+        <ContentContainer id="contact">
+          <h1 className="outline">Contact Me</h1>
+          <div className="social-links-container">
+            <p>
+              <span>
+                <FontAwesomeIcon icon={faEnvelope} /> anthonypillow18@gmail.com
+              </span>
+            </p>
+            <p>
+              <span>
+                <FontAwesomeIcon icon={faGithub} /> SquidDOTjpeg
+              </span>
+            </p>
+          </div>
+        </ContentContainer>
+      </main>
     </>
   );
 }
