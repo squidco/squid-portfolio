85c3a8e045e593bd7834d76d67e3a540a3159561
Anthony Pillow MacBook <anthonypillow18@gmail.com>
2023-08-01 13:33:04 -0700

    Created and added a logo with figma. Made a component for it.

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import logoSVG from "../assets/logo.svg"
+
+export default function Logo() {
+  return (
+    <img src={logoSVG} className='logo' />
+  )
+}
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { useState } from "react";
-import Hamburger from "../Hamburger/Hamburger";
 import NavControls from "../NavControls/NavControls";
+import Logo from "../Logo";
 import "./Nav.css";
 
 export default function Nav() {
   // renders the nav bar and nav controls component within it
   return (
     <div className="nav">
-      <p>AP</p>
+      <Logo />
 
       <NavControls />
     </div>
